7169073dd7491a801b7e0fb53832ad3e4c608f93
Matt Parlette <matthew.parlette@gmail.com>
2012-07-29 21:11:30 -0400

    Created class files for distributed work

---
+++ game.py
@@ -0,0 +1,12 @@
+import player
+
+class Game:
+  def __init__(self):
+    #Game ID: date string with a random number on the end
+    self.game_id = datetime.date
+  
+  def add_player(self,player):
+    pass
+  
+  def __repr__(self):
+    return "Game ID: "+self.game_id
